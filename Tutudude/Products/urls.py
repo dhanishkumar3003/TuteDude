bac9620ec0b5679ebb2e2e5817a6ad3c8b8a9689
dhanishkumar3003 <dhanishkumar3003@gmail.com>
2025-07-26 23:35:25 +0530

    Fixed products

--- Tutudude/Products/urls.py
+++ Tutudude/Products/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
+    path('get/', views.product_list, name='product_list'),
     path('add/', views.add_product, name='add_product'),
+    path('get/<int:pk>/', views.get_product, name='get_product'),
     path('delete/<int:pk>/', views.delete_product, name='delete_product'),
+
 ]
--- Tutudude/Products/views.py
+++ Tutudude/Products/views.py
@@ -5,13 +5,14 @@ from .models import Product
 from .serializers import ProductSerializer
 from drf_yasg.utils import swagger_auto_schema
 
-
+@swagger_auto_schema(method='get', responses={200: ProductSerializer(many=True)})
 @api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
+@swagger_auto_schema(method='post', request_body=ProductSerializer, responses={201: ProductSerializer})
 @api_view(['POST'])
 def add_product(request):
     serializer = ProductSerializer(data=request.data)
@@ -20,11 +21,23 @@ def add_product(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@swagger_auto_schema(method='get', responses={200: ProductSerializer, 404: 'Not Found'})
+@api_view(['GET'])
+def get_product(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@swagger_auto_schema(method='delete', responses={204: 'Deleted', 404: 'Not Found'})
 @api_view(['DELETE'])
 def delete_product(request, pk):
     try:
         product = Product.objects.get(pk=pk)
     except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
     product.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
